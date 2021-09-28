a841d6ee4b6995f7abf85206d63c69421067f60b
crekha1 <80155306+crekha1@users.noreply.github.com>
2021-09-28 14:16:29 +0530

    Update info.py

--- spr/modules/info.py
+++ spr/modules/info.py
@@ -18,7 +18,7 @@ __HELP__ = """
 /info [CHAT_ID/Username|USER_ID/Username]
 
 or you can use inline mode >>
-@SpamProtectionBot [CHAT_ID/Username|USER_ID/Username]
+@SpamProtection_0Bot [CHAT_ID/Username|USER_ID/Username]
 """
 
 
